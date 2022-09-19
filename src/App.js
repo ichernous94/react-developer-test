26f0979de40ada51ef6ed85c3e712e1532eda61f
Igor Chernous <ichernous94@gmail.com>
2022-09-19 17:42:58 +0300

    feat: add imports and fixed App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,80 @@
-import './App.css';
-import './styles/general.css';
+import { Component } from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter, Outlet, Routes, Route } from 'react-router-dom';
+import { Provider, connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Scandiweb Junior Developer test</h1>
-    </div>
-  );
+// Styles
+import './styles/externalStyles.css';
+import './styles/generalStyles.css';
+import './App.css';
+import { store } from './services/redux/store';
+import apolloClient from './services/graphql/apolloSetup';
+
+// Components
+import Navbar from './components/Navbar';
+import Products from './pages/ProductListing';
+
+// Routes
+import AllRoutes from './routes/mainRoutes';
+
+// Services
+import { getCategoriesAndCurrencies } from './services/graphql/requests';
+import { setCurrency } from './services/redux/currencySlice';
+
+class App extends Component {
+  state = {
+    categories: [],
+    currencies: []
+  };
+  async componentDidMount() {
+    const {
+      data: { categories, currencies }
+    } = await getCategoriesAndCurrencies();
+    this.setState(() => ({
+      categories: categories.map(({ name }) => name) || [],
+      currencies:
+        currencies.map(({ symbol, label }) => ({ symbol, label })) || []
+    }));
+    const defaultCurrency = currencies[0].symbol || '';
+    this.props.setCurrency(defaultCurrency);
+  }
+  render() {
+    if (!this.state.categories.length > 0) return <></>;
+    return (
+      <div className="App">
+        <div className="header">
+          <Navbar {...this.state} />
+        </div>
+        <div className="main-body">
+          <Outlet />
+          <Routes>
+            <Route
+              index
+              element={
+                <Products firstCategory={this.state.categories[0]} />
+              }
+            />
+            {AllRoutes}
+          </Routes>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+App = connect(null, { setCurrency })(App);
+
+class AppWithProviders extends Component {
+  render() {
+    return (
+      <ApolloProvider client={apolloClient}>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      </ApolloProvider>
+    );
+  }
+}
+export default AppWithProviders;
